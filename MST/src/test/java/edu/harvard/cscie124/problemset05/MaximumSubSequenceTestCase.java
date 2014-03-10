4895119078a3cc684b022089f62de6fe1363b025
cesarnda <cesarnda@gmail.com>
2014-03-10 13:21:52 -0400

    Update MaximumSubSequenceTestCase.java

--- MST/src/test/java/edu/harvard/cscie124/problemset05/MaximumSubSequenceTestCase.java
+++ MST/src/test/java/edu/harvard/cscie124/problemset05/MaximumSubSequenceTestCase.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 
 public class MaximumSubSequenceTestCase {
 
+	private final double DELTA = 0.01;
 	
 	@Test
 	public void getMaximumSum_With_Only_Negative_Integers(){
@@ -12,7 +13,7 @@ public class MaximumSubSequenceTestCase {
 		MaximumSubSequence maximumSubSequence = new MaximumSubSequence();
 		MaximumSubSequenceDTO maximumSubSequenceDTO = maximumSubSequence.getMaximumSum(array);
 		double sum = maximumSubSequenceDTO.getMaximumSum();
-		assertEquals(0.0, sum);
+		assertEquals(0.0, sum, DELTA);
 		assertEquals(-1, maximumSubSequenceDTO.getStartIndex());
 		assertEquals(-1, maximumSubSequenceDTO.getEndIndex());
 	}
@@ -23,9 +24,9 @@ public class MaximumSubSequenceTestCase {
 		MaximumSubSequence maximumSubSequence = new MaximumSubSequence();
 		MaximumSubSequenceDTO maximumSubSequenceDTO = maximumSubSequence.getMaximumSum(array);
 		double sum = maximumSubSequenceDTO.getMaximumSum();
-		assertEquals(6.0, sum);
+		assertEquals(6.0, sum, DELTA);
 		assertEquals(3, maximumSubSequenceDTO.getStartIndex());
-		assertEquals(4, maximumSubSequenceDTO.getEndIndex());
+		assertEquals(3, maximumSubSequenceDTO.getEndIndex());
 	}
 	
 	@Test
@@ -34,7 +35,22 @@ public class MaximumSubSequenceTestCase {
 		MaximumSubSequence maximumSubSequence = new MaximumSubSequence();
 		MaximumSubSequenceDTO maximumSubSequenceDTO = maximumSubSequence.getMaximumSum(array);
 		double sum = maximumSubSequenceDTO.getMaximumSum();
-		assertEquals(16.0, sum);
+		assertEquals(16.0, sum, DELTA);
 		assertEquals(2, maximumSubSequenceDTO.getStartIndex());
 		assertEquals(6, maximumSubSequenceDTO.getEndIndex());
 	}
+	
+	@Test
+	public void getMaximumSum_With_Maximum_Sum_Between_Negatives_And_Two_Positive_SubSequences(){
+		int[] array = new int[]{-1, -1, 3, 6, -1, -3, 11, -16, -18, 20};
+		MaximumSubSequence maximumSubSequence = new MaximumSubSequence();
+		MaximumSubSequenceDTO maximumSubSequenceDTO = maximumSubSequence.getMaximumSum(array);
+		double sum = maximumSubSequenceDTO.getMaximumSum();
+		assertEquals(20.0, sum, DELTA);
+		assertEquals(2, maximumSubSequenceDTO.getStartIndex());
+		assertEquals(6, maximumSubSequenceDTO.getEndIndex());
+	}
+	
+	
+}
+
