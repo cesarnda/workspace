daeed7ef66d0c98deffd97abac1471f7b74c36cf
cesarnda <cesarnda@gmail.com>
2014-03-21 12:24:47 -0400

    Update PrimeWitnessFinderTestCase.java
    Adding test cases for pseudo primes

--- Strassen/Strassen/src/test/java/edu/harvard/cscie124/problemset06/PrimeWitnessFinderTestCase.java
+++ Strassen/Strassen/src/test/java/edu/harvard/cscie124/problemset06/PrimeWitnessFinderTestCase.java
@@ -1,10 +1,11 @@
 package edu.harvard.cscie124.problemset06;
 
-import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
+
+import edu.harvard.cscie124.problemset06.PrimeWitnessFinder;
 
 public class PrimeWitnessFinderTestCase {
 	
@@ -16,37 +17,64 @@ public class PrimeWitnessFinderTestCase {
 	}
 	
 	@Test
-	public void findWitness_Test_With_a_2_And_p_7(){
+	public void applyFermatLittleTheorem_Test_With_a_2_And_p_7(){
 		PrimeWitnessFinder primeWitnessFinder = new PrimeWitnessFinder();
 		int reminder = primeWitnessFinder.applyFermatLittleTheorem(2, 7);
 		assertEquals(1, reminder);
 	}
 	
 	@Test
-	public void findWitness_Test_With_a_3_And_p_2(){
+	public void applyFermatLittleTheorem_Test_With_a_3_And_p_2(){
 		PrimeWitnessFinder primeWitnessFinder = new PrimeWitnessFinder();
 		int reminder = primeWitnessFinder.applyFermatLittleTheorem(3, 2);
 		assertEquals(1, reminder);
 	}
 	
 	@Test
-	public void findWitness_Test_With_a_6_And_p_2(){
+	public void applyFermatLittleTheorem_Test_With_a_6_And_p_2(){
 		PrimeWitnessFinder primeWitnessFinder = new PrimeWitnessFinder();
 		int reminder = primeWitnessFinder.applyFermatLittleTheorem(6, 2);
 		assertEquals(0, reminder);
 	}
 	
 	@Test
-	public void findWitness_Test_With_a_14_And_p_7(){
+	public void applyFermatLittleTheorem_Test_With_a_14_And_p_7(){
 		PrimeWitnessFinder primeWitnessFinder = new PrimeWitnessFinder();
 		int reminder = primeWitnessFinder.applyFermatLittleTheorem(-14, 7);
 		assertEquals(0, reminder);
 	}
 
 	@Test
-	public void findWitness_Test_With_a_150_And_p_997(){
+	public void applyFermatLittleTheorem_Test_With_a_150_And_p_997(){
 		PrimeWitnessFinder primeWitnessFinder = new PrimeWitnessFinder();
 		int reminder = primeWitnessFinder.applyFermatLittleTheorem(150,997);
+		assertEquals(1, reminder);
+	}
+	
+	@Test
+	public void test_11_Is_Prime(){
+		applyFermatLittleTheorem_Test_P_Is_Prime(11);
+	}
+	
+	
+	@Test
+	public void test_Pseudo_Prime(){
+		int[] primes = new int[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
+									53, 59, 61, 67, 71, 73, 79, 83};
+		for(int prime : primes){
+			applyFermatLittleTheorem_Test_P_Is_Prime(prime);
+		}
+	}
+	
+	public void applyFermatLittleTheorem_Test_P_Is_Prime(int p){
+		for(int a = 1; a < p; a++){
+			applyFermatLittleTheorem_Test_P_Is_Prime_With_Base_a(a, p);
+		}
+	}
+	
+	public void applyFermatLittleTheorem_Test_P_Is_Prime_With_Base_a(int a, int p){
+		PrimeWitnessFinder primeWitnessFinder = new PrimeWitnessFinder();
+		int reminder = primeWitnessFinder.applyFermatLittleTheorem(a, p);
 		assertEquals(1, reminder);
 	}
 }
