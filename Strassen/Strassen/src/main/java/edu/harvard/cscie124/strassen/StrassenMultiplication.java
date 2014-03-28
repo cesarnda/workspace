ad02258d514c268885f95d498d517f1f1316a51e
cesarnda <cesarnda@gmail.com>
2014-03-27 23:05:46 -0700

    Adding submatrix for result C

--- Strassen/Strassen/src/main/java/edu/harvard/cscie124/strassen/StrassenMultiplication.java
+++ Strassen/Strassen/src/main/java/edu/harvard/cscie124/strassen/StrassenMultiplication.java
@@ -10,7 +10,7 @@ public class StrassenMultiplication {
 	 * @param b
 	 * @return
 	 */
-	public double[][] multiplyUsingNaiveImplementation(double[][] matrixA, double[][] matrixB){
+	public double[][] multiply(double[][] matrixA, double[][] matrixB){
 		int n = matrixA.length;
 		
 		double[][] matrixC = new double[n][n];
@@ -42,6 +42,19 @@ public class StrassenMultiplication {
         double[][] subMatrixS8  = add(subMatrixB21, subMatrixB22);
         double[][] subMatrixS9  = subtract(subMatrixA11, subMatrixA21);
         double[][] subMatrixS10 = add(subMatrixB11, subMatrixB12);
+        
+        double[][] subMatrixP1 = multiply(subMatrixA11, subMatrixS1);
+        double[][] subMatrixP2 = multiply(subMatrixS2, subMatrixB22);
+        double[][] subMatrixP3 = multiply(subMatrixS3, subMatrixB11);
+        double[][] subMatrixP4 = multiply(subMatrixA22, subMatrixS4);
+        double[][] subMatrixP5 = multiply(subMatrixS5, subMatrixS6);
+        double[][] subMatrixP6 = multiply(subMatrixS7, subMatrixS8);
+        double[][] subMatrixP7 = multiply(subMatrixS9, subMatrixS10);
+        
+        double[][] subMatrixC11 = add(subtract(add(subMatrixP5, subMatrixP4), subMatrixP2), subMatrixP6);
+        double[][] subMatrixC12 = add(subMatrixP1, subMatrixP2);
+        double[][] subMatrixC21 = add(subMatrixP3, subMatrixP4);
+        double[][] subMatrixC22 = subtract(subtract(add(subMatrixP5, subMatrixP1), subMatrixP3), subMatrixP7);
         
 		return matrixC;
 	}
