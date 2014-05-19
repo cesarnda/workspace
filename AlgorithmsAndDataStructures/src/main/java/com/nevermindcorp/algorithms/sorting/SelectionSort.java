947dd401aec115e019a8725935e360eaf26972f3
cesarnda <cesarnda@gmail.com>
2014-05-18 22:07:02 -0700

    Generic implementation of Algorithms

---
+++ AlgorithmsAndDataStructures/src/main/java/com/nevermindcorp/algorithms/sorting/SelectionSort.java
@@ -0,0 +1,5 @@
+package com.nevermindcorp.algorithms.sorting;
+
+public class SelectionSort {
+
+}
---
+++ AlgorithmsAndDataStructures/src/main/java/com/nevermindcorp/algorithms/sorting/Sorting.java
@@ -0,0 +1,9 @@
+package com.nevermindcorp.algorithms.sorting;
+
+import java.util.List;
+
+public interface Sorting {
+
+	public <T>List<T> sort(List<T> list);
+	
+}
